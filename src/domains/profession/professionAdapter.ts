fa204d5869d9079e6f9f92c891dab39fee13cf7d
dzgierski19 <dzgierskigit@gmail.com>
2024-10-15 01:45:31 +0200

    created basic methods for adding user and getting all users from db

--- src/domains/profession/professionAdapter.ts
+++ src/domains/profession/professionAdapter.ts
@@ -1,3 +1,29 @@
+import { prisma } from "../../../prisma/prisma";
+import { Level, Role } from "./professionTypes";
+
 interface IProfessionAdapter {}
 
-export class ProfessionAdapter implements IProfessionAdapter {}
+export class ProfessionAdapter implements IProfessionAdapter {
+  async createOne(
+    name: string,
+    role: Role,
+    level: Level,
+    phoneNumber: number,
+    email: string
+  ) {
+    await prisma.profession.create({
+      data: {
+        name: name,
+        role: role,
+        workerLevel: level,
+        phoneNumber: phoneNumber,
+        email: email,
+      },
+    });
+  }
+
+  async getAll() {
+    const workers = await prisma.profession.findMany({});
+    console.log(workers);
+  }
+}
