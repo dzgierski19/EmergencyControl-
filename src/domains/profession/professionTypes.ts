e223e4912c15a7a92810fb03dc49712b00ae15af
dzgierski19 <dzgierskigit@gmail.com>
2024-10-15 13:32:01 +0200

    added Profession, create and update types

--- src/domains/profession/professionTypes.ts
+++ src/domains/profession/professionTypes.ts
@@ -2,6 +2,7 @@ export const ROLES = {
   POLICEMAN: "POLICEMAN",
   FIREFIGHTER: "FIREFIGHTER",
   PARAMEDIC: "PARAMEDIC",
+  SOLDIER: "SOLDIER",
 } as const;
 
 export type Role = (typeof ROLES)[keyof typeof ROLES];
@@ -13,3 +14,23 @@ export const LEVELS = {
 } as const;
 
 export type Level = (typeof LEVELS)[keyof typeof LEVELS];
+
+export type Profession = {
+  id: string;
+  name: string;
+  role: Role;
+  workerLevel: Level;
+  email: string;
+  phoneNumber: number;
+  currentEventId: string | null;
+  createdAt: Date;
+  deletedAt: Date | null;
+  updatedAt: Date | null;
+};
+
+export type ProfessionToCreate = Omit<
+  Profession,
+  "id" | "createdAt" | "deletedAt" | "updatedAt"
+>;
+
+export type ProfessionToUpdate = Partial<ProfessionToCreate>;
