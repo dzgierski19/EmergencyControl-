dac0898597fdede729814af0b2d95e8c066823dd
dzgierski19 <dzgierskigit@gmail.com>
2024-10-15 11:40:24 +0200

    professionAdapter to handle CRUD operations on profession model

--- src/domains/profession/professionAdapter.ts
+++ src/domains/profession/professionAdapter.ts
@@ -1,29 +1,43 @@
+import { Prisma, PrismaClient } from "@prisma/client";
 import { prisma } from "../../../prisma/prisma";
-import { Level, Role } from "./professionTypes";
+import { Level, Profession, Role } from "./professionTypes";
 
-interface IProfessionAdapter {}
+export interface IProfessionAdapter {
+  createOne(data: any): Promise<void>;
+  getAll(): Promise<Profession[]>;
+  getOne(id: string): Promise<Profession | null>;
+  deleteOne(id: string): Promise<void>;
+  updateOne(id: string, data: any): Promise<void>;
+}
 
 export class ProfessionAdapter implements IProfessionAdapter {
-  async createOne(
-    name: string,
-    role: Role,
-    level: Level,
-    phoneNumber: number,
-    email: string
-  ) {
+  async createOne(data: any) {
     await prisma.profession.create({
-      data: {
-        name: name,
-        role: role,
-        workerLevel: level,
-        phoneNumber: phoneNumber,
-        email: email,
-      },
+      data: data,
     });
   }
 
   async getAll() {
-    const workers = await prisma.profession.findMany({});
-    console.log(workers);
+    const all = await prisma.profession.findMany({
+      where: { deletedAt: null },
+    });
+    console.log(all);
+    return all;
+  }
+
+  async getOne(id: string) {
+    const one = await prisma.profession.findUnique({ where: { id } });
+    return one;
+  }
+
+  async deleteOne(id: string) {
+    await prisma.profession.update({
+      where: { id: id },
+      data: { deletedAt: new Date() },
+    });
+  }
+
+  async updateOne(id: string, data: any) {
+    await prisma.profession.update({ where: { id: id }, data: data });
   }
 }
