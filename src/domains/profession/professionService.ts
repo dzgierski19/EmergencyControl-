c41f79a6906a1bbbfcd17a3f55ab6d18e191ac88
dzgierski19 <dzgierskigit@gmail.com>
2024-10-16 21:13:13 +0200

    added method getting professions by level and to compare levels

--- src/domains/profession/professionService.ts
+++ src/domains/profession/professionService.ts
@@ -1,7 +1,22 @@
 import { IProfessionAdapter } from "./professionAdapter";
-import { ProfessionToCreate, ProfessionToUpdate } from "./professionTypes";
+import {
+  Level,
+  NUMBER_LEVEL_MAPPER,
+  Profession,
+  ProfessionToCreate,
+  ProfessionToUpdate,
+} from "./professionTypes";
 
-export class professionService {
+export interface IProfessionService {
+  createProfession(data: ProfessionToCreate): Promise<void>;
+  getAllProfessions(): Promise<Profession[]>;
+  getProfession(id: string): Promise<Profession>;
+  getProfessionsByLevel(level: Level): Promise<Profession[]>;
+  deleteProfession(id: string): Promise<void>;
+  updateProfession(id: string, data: ProfessionToUpdate): Promise<void>;
+}
+
+export class ProfessionService implements IProfessionService {
   constructor(private readonly professionAdapter: IProfessionAdapter) {}
 
   async createProfession(data: ProfessionToCreate) {
@@ -9,12 +24,19 @@ export class professionService {
   }
 
   async getAllProfessions() {
-    return this.professionAdapter.getAll();
+    return await this.professionAdapter.getAll();
   }
 
   async getProfession(id: string) {
     const profession = await this.professionAdapter.getOne(id);
     return profession;
+  }
+
+  async getProfessionsByLevel(level: Level) {
+    const all = await this.getAllProfessions();
+    return all.filter((profession) => {
+      return this.compareLevels(profession.workerLevel, level);
+    });
   }
 
   async deleteProfession(id: string) {
@@ -25,5 +47,11 @@ export class professionService {
   async updateProfession(id: string, data: ProfessionToUpdate) {
     await this.getProfession(id);
     await this.professionAdapter.updateOne(id, data);
+  }
+
+  private compareLevels(professionLevel: Level, eventLevel: Level): boolean {
+    return (
+      NUMBER_LEVEL_MAPPER[professionLevel] >= NUMBER_LEVEL_MAPPER[eventLevel]
+    );
   }
 }
