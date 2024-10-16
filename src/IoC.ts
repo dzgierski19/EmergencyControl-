54af39ba89dbd4527280c40848c28159163bee4a
dzgierski19 <dzgierskigit@gmail.com>
2024-10-16 23:39:44 +0200

    app implementation

---
+++ src/IoC.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { ProfessionAdapter } from "./domains/profession/professionAdapter";
+import { LEVELS } from "./domains/event/eventTypes";
+import { ProfessionService } from "./domains/profession/professionService";
+import { EventAdapter } from "./domains/event/eventAdapter";
+import { EventService } from "./domains/event/eventService";
+import { EventController } from "./domains/event/eventController";
+
+export const professionAdapter = new ProfessionAdapter();
+export const professionService = new ProfessionService(professionAdapter);
+export const eventAdapter = new EventAdapter();
+export const eventService = new EventService(professionService, eventAdapter);
+export const eventController = new EventController(
+  eventService,
+  professionService
+);
