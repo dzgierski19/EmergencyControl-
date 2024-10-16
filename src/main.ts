ec641174731e70d9b8bae75e5f2fb9bcca118d3c
dzgierski19 <dzgierskigit@gmail.com>
2024-10-16 23:35:55 +0200

    added eventRouter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import { eventRouter } from "./domains/event/eventRouter";
 
 export const app = express();
-const port = 8500;
+const port = 8000;
 
 app.use(express.json());
+
+app.use(eventRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
