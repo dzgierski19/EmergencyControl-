1498338b9a88a78a66611052bef012e41f2247d8
dzgierski19 <dzgierskigit@gmail.com>
2024-10-18 10:45:05 +0200

    added professionRouter

--- src/domains/event/eventRouter.ts
+++ src/domains/event/eventRouter.ts
@@ -8,6 +8,17 @@ eventRouter.get("/events", eventController.getEvents);
 
 eventRouter.get("/events/:eventId", eventController.getEvent);
 
+eventRouter.get(
+  "/events/profession/:professionId",
+  eventController.getAllEventsForProfession
+);
+
 eventRouter.post("/events", eventController.createEvent);
 
 eventRouter.post("/events/:eventId", eventController.addWorkerToEvent);
+
+eventRouter.put("/events/:eventId/accept-event", eventController.acceptEvent);
+
+eventRouter.put("/events/:eventId/finish-event", eventController.finishEvent);
+
+eventRouter.put("/events/:eventId/cancel-event", eventController.cancelEvent);
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { eventRouter } from "./domains/event/eventRouter";
+import { professionRouter } from "./domains/profession/professionRouter";
 
 export const app = express();
 const port = 8000;
@@ -7,6 +8,8 @@ const port = 8000;
 app.use(express.json());
 
 app.use(eventRouter);
+
+app.use(professionRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
